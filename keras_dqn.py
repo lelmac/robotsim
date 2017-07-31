fca7756aa4df16e47cda23d40ddc82e80cd42b06
TheOssi <lenk.max@googlemail.com>
2017-07-31 09:18:59 +0200

    test

--- keras_dqn.py
+++ keras_dqn.py
@@ -1,6 +1,6 @@
 import numpy as np
 import gym
-
+from datetime import datetime
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten
 from keras.optimizers import Adam
@@ -33,21 +33,24 @@ print(model.summary())
 
 # Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
 # even the metrics!
-memory = SequentialMemory(limit=10000000, window_length=1)
+memory = SequentialMemory(limit=1000000, window_length=1)
 policy = BoltzmannQPolicy()
+
 dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=50,
                target_model_update=1e-2, policy=policy)
+             
 dqn.compile(Adam(lr=1e-3), metrics=['mae'])
-log_filename = 'dqn_{}_log.json'.format(ENV_NAME)
+date = str(datetime.now())
+log_filename = 'dqn_{}_{}_log.json'.format(ENV_NAME,date)
 callbacks = [FileLogger(log_filename, interval=25)]
-
+#dqn.load_weights('dqn_{}_weights.h5f'.format(ENV_NAME))  
 # Okay, now it's time to learn something! We visualize the training here for show, but this
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
-dqn.fit(env, nb_steps=10000000, visualize=False, verbose=2,callbacks=callbacks)
+dqn.fit(env, nb_steps=1000000, visualize=False, verbose=2,callbacks=callbacks)
 
 # After training is done, we save the final weights.
-dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+#dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
 
 # Finally, evaluate our algorithm for 5 episodes.
-#dqn.test(env, nb_episodes=5, visualize=True)
+#dqn.test(env, nb_episodes=10, visualize=True)
